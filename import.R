812b5b9df3062031cea66480dff9b67bbe0a369a
diminou <ugyz@hotmail.com>
2015-03-01 19:05:08 +0100

    quelques methodes inutiles commentees

--- import.R
+++ import.R
@@ -295,106 +295,106 @@ for(i in 34:37){ # vérifier qu'il y a bien 37 fichiers créés
   saveBddBisPart(i)
 }
 
-has_angle_brackets <- cmpfun( function( string ) {
-  return ( grepl ("<", string, fixed = T) & grepl(">", string, fixed = T))
-})
-
-longer_than_4 <- cmpfun( function( string ){
-  return ( nchar(string) > 4 )
-})
-
-non_empty_string <- cmpfun( function( string ){
-  return ( nchar(string) > 0 )
-})
-
-harvest4 <- cmpfun(function(string_array) {
-  return(Filter(longer_than_4, string_array))
-})
-
-harvest <- cmpfun(function(string_array) {
-  return(Filter(non_empty_string, string_array))
-})
-
-has_fixed_pattern <- cmpfun( function(pattern, string) {
-  return(grepl(pattern, string, fixed = T))
-})
-
-is_category_resource <- cmpfun(function(string) {
-  return(has_fixed_pattern("resource/Catégorie:", string))
-})
-
-is_article <- cmpfun(function(string) {
-  return((!has_fixed_pattern("resource/Catégorie:", string))&has_fixed_pattern("resource/", string))
-})
-
-
-
-splitTripleXml <- cmpfun( function(line) {
-  parts <- harvest4(strsplit(line, "[<]|[>]")[[1]])
-  if(length(parts) < 3) {
-    throw("invalid line")
-  }
-  return(parts)
-})
-
-extract_article <- cmpfun(function(string) {
-  parts <- splitTripleXml(string)
-  part <- Filter(is_article, parts)
-  elements <- harvest(strsplit(part, "/")[[1]])
-  return (tail(elements, 1))
-})
-
-extract_category <- cmpfun(function(string){
-  parts <- splitTripleXml(string)
-  part <- tail(Filter(is_category_resource, parts), 1)
-  elements <- harvest(strsplit(part, "/")[[1]])
-  atoms <- harvest(strsplit(tail(elements, 1), ":")[[1]])
-  print("atoms :")
-  print(atoms)
-  return(tail(atoms, 1))
-})
-
-conn <- file(paste(HOME, fileName, sep = "/"), open = "r")
-addConstraint(db, "article", "title")
-addConstraint(db, "category", "label")
-line <- readLines(conn, 1)
-line <- readLines(conn, 1)
-query1 <- extractArticleMQ(line)
-query2 <- extractCategoryMQ(line)
-query3 <- extractIs_underMQ(line)
-print(query1)
-print(query2)
-print(query3)
-cypher(db, query1)
-cypher(db, query2)
-cypher(db, query3)
-#ca <- createNode(db, "article", title = extractArticle(line))
-#cc <- createNode(db, "category", label = extractCategory(line))
-#createRel(ca, "is_under", cc )
-close(conn)
-
-
-
-
-
-conn <- file(paste(HOME, fileName, sep = "/"), open = "r")
-counter100 <- 0
-counter <- 0
-while(length(line <- readLines(conn, 1)) > 0 ){
-    tryCatch({
-      query1 <- extractArticleMQ(line)
-      query2 <- extractCategoryMQ(line)
-      query3 <- extractIs_underMQ(line)
-      cypher(db, query1)
-      cypher(db, query2)
-      cypher(db, query3)
-    }, error=function(e){})
-    counter <- counter + 1
-    if(counter%/%100 > counter100){
-      print(counter)
-      counter100 <- counter %/% 100
-    }
-}
-close(conn)
+# has_angle_brackets <- cmpfun( function( string ) {
+#   return ( grepl ("<", string, fixed = T) & grepl(">", string, fixed = T))
+# })
+# 
+# longer_than_4 <- cmpfun( function( string ){
+#   return ( nchar(string) > 4 )
+# })
+# 
+# non_empty_string <- cmpfun( function( string ){
+#   return ( nchar(string) > 0 )
+# })
+# 
+# harvest4 <- cmpfun(function(string_array) {
+#   return(Filter(longer_than_4, string_array))
+# })
+# 
+# harvest <- cmpfun(function(string_array) {
+#   return(Filter(non_empty_string, string_array))
+# })
+# 
+# has_fixed_pattern <- cmpfun( function(pattern, string) {
+#   return(grepl(pattern, string, fixed = T))
+# })
+# 
+# is_category_resource <- cmpfun(function(string) {
+#   return(has_fixed_pattern("resource/Catégorie:", string))
+# })
+# 
+# is_article <- cmpfun(function(string) {
+#   return((!has_fixed_pattern("resource/Catégorie:", string))&has_fixed_pattern("resource/", string))
+# })
+# 
+# 
+# 
+# splitTripleXml <- cmpfun( function(line) {
+#   parts <- harvest4(strsplit(line, "[<]|[>]")[[1]])
+#   if(length(parts) < 3) {
+#     throw("invalid line")
+#   }
+#   return(parts)
+# })
+# 
+# extract_article <- cmpfun(function(string) {
+#   parts <- splitTripleXml(string)
+#   part <- Filter(is_article, parts)
+#   elements <- harvest(strsplit(part, "/")[[1]])
+#   return (tail(elements, 1))
+# })
+# 
+# extract_category <- cmpfun(function(string){
+#   parts <- splitTripleXml(string)
+#   part <- tail(Filter(is_category_resource, parts), 1)
+#   elements <- harvest(strsplit(part, "/")[[1]])
+#   atoms <- harvest(strsplit(tail(elements, 1), ":")[[1]])
+#   print("atoms :")
+#   print(atoms)
+#   return(tail(atoms, 1))
+# })
+# 
+# conn <- file(paste(HOME, fileName, sep = "/"), open = "r")
+# addConstraint(db, "article", "title")
+# addConstraint(db, "category", "label")
+# line <- readLines(conn, 1)
+# line <- readLines(conn, 1)
+# query1 <- extractArticleMQ(line)
+# query2 <- extractCategoryMQ(line)
+# query3 <- extractIs_underMQ(line)
+# print(query1)
+# print(query2)
+# print(query3)
+# cypher(db, query1)
+# cypher(db, query2)
+# cypher(db, query3)
+# #ca <- createNode(db, "article", title = extractArticle(line))
+# #cc <- createNode(db, "category", label = extractCategory(line))
+# #createRel(ca, "is_under", cc )
+# close(conn)
+# 
+# 
+# 
+# 
+# 
+# conn <- file(paste(HOME, fileName, sep = "/"), open = "r")
+# counter100 <- 0
+# counter <- 0
+# while(length(line <- readLines(conn, 1)) > 0 ){
+#     tryCatch({
+#       query1 <- extractArticleMQ(line)
+#       query2 <- extractCategoryMQ(line)
+#       query3 <- extractIs_underMQ(line)
+#       cypher(db, query1)
+#       cypher(db, query2)
+#       cypher(db, query3)
+#     }, error=function(e){})
+#     counter <- counter + 1
+#     if(counter%/%100 > counter100){
+#       print(counter)
+#       counter100 <- counter %/% 100
+#     }
+# }
+# close(conn)
 
 
