70dfb495ddb5599e744163f4ad1d80cf7f3be053
BenoitHugonnard <benoit.hugonnard@gmail.com>
2015-03-02 11:55:11 +0100

    esa.R ajouté

---
+++ esa.R
@@ -0,0 +1,13 @@
+# Voici le code qui devrait permettre d'implémenter l'ESA
+
+# Partie 1 : Algorithme en langue courante
+
+# Vocabulaire :
+# - mot clé : mot lemmatisé, unicité dans la base de données
+# - abstract : ensemble de mots définissant un sujet
+# - concept : article Wikipedia défini par son nom et comprenant un abstract
+# - categorie : noeud ayant des liaisons avec certains concepts
+
+# Algorithme à implémenter :
+# -Importer la base données en graph qui comprends les mots-clés, les concepts et les catégories
+# 
