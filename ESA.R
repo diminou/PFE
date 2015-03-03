5cb75bb046b993c645b4c2b946461db7fae92143
Nollain <quentin.nollet@hotmail.fr>
2015-03-03 12:01:42 +0100

    utiliation requete neo4j dans ESA

--- ESA.R
+++ ESA.R
@@ -1,6 +1,11 @@
 
-
-
+# Libaiies utiles pour les fction utilsées venant de requetesNEo4j.R et import.R 
+library(tm)
+library(SnowballC)
+library(RTextTools)
+library(RNeo4j)
+#install.packages("devtools")
+# devtools::install_github("nicolewhite/RNeo4j")
 
 
 #  tf time frequency du mot dans le doc, nb nb de doc, df document frequency du mot
@@ -16,33 +21,34 @@ TFIDF_norm <- function(tfidf, r){
 
 
 sim_cos <- function(v1, v2){
-  num=0
-  part1= 0
-  part2 = 0
-  for(i=1:length(v1)){
+  num <- 0
+  part1 <-  0
+  part2 <-  0
+  for(i in 1:length(v1)){
     num= num + v1[i]*v2[i]
     part1 = part1 + v1[i]*v1[i]
     part2 = part2 + v2[i]*v2[i]
   }
   
   denom = sqrt(part1)*sqrt(part2)
-  res= num /denom
+  res= (num /denom)
   return (res)
 }
 
 
 
 # nb de doc 
-n <- 2 
+
 
 # retourne le vecteur semantique du mot mot (vecteur numérique)
-MotSemantInterp <- function(mot, listeDoc){
-  nb = 2 # pour l'exemple
+MotSemantInterp <- function(mot){
+  ArtFrWor=getArticlesFromWord(mot)
+  n = nrow(ArtFrWor)
+  df = n 
   s <- rep(0,n)
-  df # récupérer à partir du mot depuis code eve
-  for(i=1:n){
-    tf # récupéré à partir du doc depuis code eve
-    s[i] <- TFIDF(tf, nb, df)
+  for(i in 1:n){
+    tf = 1 + log(ArtFrWor[i,2]) 
+    s[i] <- TFIDF(tf, 1, df)
   }
   return s
 }
