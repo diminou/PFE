a460ba4241141034f6ec706abad50b2eb66cc315
diminou <ugyz@hotmail.com>
2015-03-16 02:49:56 +0100

    Exportation du graphe elague

--- requetesExpension.R
+++ requetesExpension.R
@@ -33,6 +33,77 @@ getCategoriesAround <- function(category_label,buffer){
   return (result)
 }
 
+getArticlesFromCategory <- function(category_label) {
+  query <- paste("match (c:category {label: '", escapeApostrophes(category_label), "'}) <-- (a:article) return a", sep = "")
+  result <- sapply(getNodes(db, query), function(x) fixEncoding(x$title))
+  return(result)
+}
+
+getWordsFromArticle <- function(article_title) {
+  query <- paste("match (a:article {title: '", escapeApostrophes(article_title), "'}) -[r]-> (w:word) return a.title, r.count, w.stem", sep = "")
+  result <- cypher(db, query)
+  result$w.stem <- sapply(result$w.stem, fixEncoding)
+  return(result)
+}
+
+getWordsFromCats <- function(category_label, depth) {
+  query <- paste ("match (c:category {label:'",
+                  escapeApostrophes(category_label),
+                  "'})<-[r1 *0..",
+                  depth,
+                  "]-(cat:category)<--(a:article)-[r]->(w:word) return cat.label, a.title, r.count, w.stem",
+                      sep = "")
+  result <- cypher(db, query)
+  result$w.stem <- sapply(result$w.stem, fixEncoding)
+  result$a.title <- sapply(result$a.title, fixEncoding)
+  result$cat.label <- sapply(result$cat.label, fixEncoding)
+  return(result)
+}
+
+getWordsFromCats25K <- function(category_label, depth, offset_over_25K) {
+  offset <- 25000 * offset_over_25K
+  query <- paste ("match (c:category {label:'",
+                  escapeApostrophes(category_label),
+                  "'})<-[r1 *0..",
+                  depth,
+                  "]-(cat:category)<--(a:article)-[r]->(w:word) return cat.label, a.title, r.count, w.stem ",
+                  "order by cat.label desc skip ",
+                  offset,
+                  " limit 25000",
+                  sep = "")
+  result <- cypher(db, query)
+  result$w.stem <- sapply(result$w.stem, fixEncoding)
+  result$a.title <- sapply(result$a.title, fixEncoding)
+  result$cat.label <- sapply(result$cat.label, fixEncoding)
+  return(result)
+}
+
+writeResultsToCsv <- function(data, path) {
+  write.csv(data, file = path, row.names = F, fileEncoding = "UTF-8")
+}
+
+writeAllFromCat25K <- function(category_label, depth) {
+  dir = paste(HOME,".pfe", category_label, sep="/")
+  dir.create(dir)
+  offset_count <- 0
+  flag = T
+  while(flag) {
+    path <- paste(dir, "/", category_label, offset_count, sep = "")
+    path <- paste(path, "csv", sep = ".")
+    data <- getWordsFromCats25K(category_label, depth, offset_count)
+    if(is.null(dim(data))){
+      flag = F
+    } else {
+      writeResultsToCsv(data, path)
+      offset_count <- offset_count + 1
+    }
+  }  
+}
+
+writeAllFromCat25K('Service_public', 2)
+writeAllFromCat25K('Métier_du_bâtiment', 3)
+writeAllFromCat25K('Type_de_commerce', 4)
+
 writeCategoriesCsv <- function(fileName,categories){
   filePath <- paste(paste(paste(HOME,"/.pfe/",sep=""),fileName,sep=""),".csv",sep="")
   write.table(categories, file = filePath,quote=FALSE,row.names = FALSE,col.names=FALSE)
