072c7fd5dbb06d2456e59bc0c8b7d1235989b3a2
diminou <ugyz@hotmail.com>
2015-03-19 17:06:55 +0100

    recherche des categories cibles a partir d'une requete

--- rechercheCatCible.R
+++ rechercheCatCible.R
@@ -14,6 +14,7 @@ library(tm)
 library(SnowballC)
 library(RTextTools)
 source(paste(PFE, "ESA.R", sep = "/"))
+source(paste(PFE, "requetesNeo4j.R", sep = "/"))
 
 ### Connexion avec Neo4j ###
 db = startGraph("127.0.0.1:7474/db/data/")
@@ -21,15 +22,32 @@ db = startGraph("127.0.0.1:7474/db/data/")
 retrieveShortestPath <- function(title1, title2) {
   query <- paste("match (a:article {title:'", escapeApostrophes(title1),
                  "'}), (b:article {title:'",
-                 escapeApostroples(title2),
-                 "'}) match p = allShortestPaths((a)-[:linked *]-(b)) return nodes(p).code")
+                 escapeApostrophes(title2),
+                 "'}) match p = allShortestPaths((a)-[:linked *]-(b))
+                  unwind nodes(p)  as cd
+                  with collect(cd.code) as res
+                  unwind res as r
+                  return r", sep = "")
   return(cypher(db, query))
 }
 
 retrieveMostPertinentPath <- function(title1, title2) {
   query <- paste("match (a:article {title:'", escapeApostrophes(title1),
                  "'}), (b:article {title:'",
-                 escapeApostroples(title2),
-                 "'}) match p (a)-[:linked *1..4]-(b) return p, reduce(acc = 1.0, r in relationships(p) | acc * tofloat(r.pertinence)) as weight order by weight desc")
-  return(cypher(db, query))
+                 escapeApostrophes(title2),
+                 "'})
+                 match p = allShortestPaths((a)-[:linked *]-(b))
+                 with  p as shortestPath, reduce(accum = 1.0, r in relationships(p) | accum * tofloat(r.pertinence)) as weight
+                 order by weight desc
+                 unwind nodes(shortestPath) as pp
+                 return distinct(pp.code), weight", sep = "")
+  result <- cypher(db,query)
+  if(is.null(result)) {
+    return(NULL)
+  }
+  return(na.omit(result))
 }
+
+
+
+retrieveMostPertinentPath("Kaufhaus_des_Westens", "Visby")
